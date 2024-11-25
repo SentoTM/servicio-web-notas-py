a24415059f5bc19804627dc664f33d7075379dde
SentoTM <vicent.tormo@gmail.com>
2024-11-25 13:10:22 +0100

    GET, PUT y DELETE

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -7,14 +7,14 @@ class gesNotas(BaseHTTPRequestHandler):
         gesnotas.ges_db = ges_db
         super().__init__(*args, **kwargs)
     
-    ##Función para enviar una respuesta al cliente después de procesar una petición
+    #Función para enviar una respuesta al cliente después de procesar una petición
     def _send_response(gesnotas, status_code, data):
         gesnotas.send_response(status_code)
         gesnotas.send_header('Content-type', 'application/json')
         gesnotas.end_headers()
         gesnotas.wfile.write(json.dumps(data).encode())
     
-    ##Función para manejar las peticiones POST
+    #Función para manejar las peticiones POST
     def do_POST(gesnotas):
         if gesnotas.path == '/notas':
             content_length = int(gesnotas.headers['Content-Length'])
@@ -26,7 +26,47 @@ class gesNotas(BaseHTTPRequestHandler):
             else:
                 gesnotas._send_response(400, {'error': 'Faltan campos obligatorios'})
 
+    def do_GET(gesnotas):
+        parsed_url = urlparse(gesnotas.path)
+        partes_url = parsed_url.path.split('/')
+        #Si la ruta es /notas, devolvemos todas las notas
+        if parsed_url.path == '/notas':
+            notas = gesnotas.ges_db.leer_notas()
+            gesnotas._send_response(200, notas)
+        elif len(partes_url) == 3 and partes_url[1] == 'notas':
+            id_nota = int(partes_url[2])
+            nota = gesnotas.ges_db.leer_nota(id_nota)
+            if nota:
+                gesnotas._send_response(200, nota)
+            else:
+                gesnotas._send_response(404, {'error': 'Nota no encontrada'})
+    
+    def do_PUT(gesnotas):
+        parsed_url = urlparse(gesnotas.path)
+        partes_url = parsed_url.path.split('/')
 
+        if len(partes_url) == 3 and partes_url[1] == 'notas':
+            id_nota = int(partes_url[2])
+            contenido_tam = int(gesnotas.headers['Content-Length'])
+            datos_modificacion = json.loads(gesnotas.rfile.read(contenido_tam))
 
+            if 'titulo' in datos_modificacion and 'descripcion' in datos_modificacion:
+                if gesnotas.ges_db.actualizar_nota(id_nota, datos_modificacion['titulo'], datos_modificacion['descripcion']):
+                    gesnotas._send_response(200, {'mensaje': 'Nota actualizada'})
+                else:
+                    gesnotas._send_response(404, {'error': 'Nota no encontrada'})
+            else:
+                gesnotas._send_response(400, {'error': 'Faltan campos obligatorios'})   
+
+    def do_DELETE(gesnotas):
+        parsed_url = urlparse(gesnotas.path)
+        partes_url = parsed_url.path.split('/')
+
+        if len(partes_url) == 3 and partes_url[1] == 'notas':
+            id_nota = int(partes_url[2])
+            if gesnotas.ges_db.eliminar_nota(id_nota):
+                gesnotas._send_response(200, {'mensaje': 'Nota eliminada'})
+            else:
+                gesnotas._send_response(404, {'error': 'Nota no encontrada'})
 
     
