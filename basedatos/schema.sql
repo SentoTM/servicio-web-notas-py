b7341624393ad61e53a48c164051e8f2f2524b64
SentoTM <vicent.tormo@gmail.com>
2024-11-26 11:22:46 +0100

    correccion schema

--- basedatos/schema.sql
+++ basedatos/schema.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS notas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo TEXT NOT NULL,
-    contenido TEXT NOT NULL,
+    descripcion TEXT NOT NULL,
     fecha_creacion TEXT NOT NULL,
     ultima_modificacion TEXT NOT NULL
-));
+);
